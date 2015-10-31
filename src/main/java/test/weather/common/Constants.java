4b4017fba350e248fed4a0e9c90b5f0a1e0f85c8
Ningwei Dong <nwdong@gmail.com>
2015-10-31 16:54:53 +1100

    added CityController and test class

--- src/main/java/test/weather/common/Constants.java
+++ src/main/java/test/weather/common/Constants.java
@@ -2,7 +2,9 @@ package test.weather.common;
 
 public interface Constants {
 	
-	public static final String CURRENT_WEATHER_URI_ROOT = "/rest";
+	public static final String CURRENT_WEATHER_URI_ROOT = "/current";
+	
+	public static final String CITY_LIST_URI = "/cities";
 	
 	public static final int CITY_NAME_LENGTH_MAX = 50;
 	
