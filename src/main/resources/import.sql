59a0ced74e0c0319c49b232edc0ee29255f57f1c
Ningwei Dong <nwdong@gmail.com>
2015-11-11 13:05:01 +1100

    add comments into index.html and update war file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,18 +1,18 @@
-insert into cities (id, name) values (1, 'Adelaide');
-insert into cities (id, name) values (2, 'Brisbane');
-insert into cities (id, name) values (3, 'Canberra');
-insert into cities (id, name) values (4, 'Darwin');
-insert into cities (id, name) values (5, 'Hobart');
-insert into cities (id, name) values (6, 'Melbourne');
-insert into cities (id, name) values (7, 'Perth');
-insert into cities (id, name) values (8, 'Sydney');
+insert into cities (id, name) values (1, 'Sydney');
+insert into cities (id, name) values (2, 'Melbourne');
+insert into cities (id, name) values (3, 'Adelaide');
+insert into cities (id, name) values (4, 'Brisbane');
+insert into cities (id, name) values (5, 'Canberra');
+insert into cities (id, name) values (6, 'Darwin');
+insert into cities (id, name) values (7, 'Hobart');
+insert into cities (id, name) values (8, 'Perth');
 
 
-insert into open_weather_cities (city_id, open_weather_city_id) values (1, 2063059);
-insert into open_weather_cities (city_id, open_weather_city_id) values (2, 2174003);
-insert into open_weather_cities (city_id, open_weather_city_id) values (3, 2172517);
-insert into open_weather_cities (city_id, open_weather_city_id) values (4, 7839402);
-insert into open_weather_cities (city_id, open_weather_city_id) values (5, 7839672);
-insert into open_weather_cities (city_id, open_weather_city_id) values (6, 7839805);
-insert into open_weather_cities (city_id, open_weather_city_id) values (7, 2153391);
-insert into open_weather_cities (city_id, open_weather_city_id) values (8, 6619279);
+insert into open_weather_cities (city_id, open_weather_city_id) values (1, 6619279);
+insert into open_weather_cities (city_id, open_weather_city_id) values (2, 7839805);
+insert into open_weather_cities (city_id, open_weather_city_id) values (3, 2063059);
+insert into open_weather_cities (city_id, open_weather_city_id) values (4, 2174003);
+insert into open_weather_cities (city_id, open_weather_city_id) values (5, 2172517);
+insert into open_weather_cities (city_id, open_weather_city_id) values (6, 7839402);
+insert into open_weather_cities (city_id, open_weather_city_id) values (7, 7839672);
+insert into open_weather_cities (city_id, open_weather_city_id) values (8, 2153391);
