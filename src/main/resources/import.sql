5c35cf08e90632a7606d1c6490850356394aa638
Ningwei Dong <nwdong@gmail.com>
2015-11-11 13:35:36 +1100

    add comment into import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+/*id is defined for internal use*/
 insert into cities (id, name) values (1, 'Sydney');
 insert into cities (id, name) values (2, 'Melbourne');
 insert into cities (id, name) values (3, 'Adelaide');
@@ -7,7 +8,7 @@ insert into cities (id, name) values (6, 'Darwin');
 insert into cities (id, name) values (7, 'Hobart');
 insert into cities (id, name) values (8, 'Perth');
 
-
+/*city_id is got from openweather website*/
 insert into open_weather_cities (city_id, open_weather_city_id) values (1, 6619279);
 insert into open_weather_cities (city_id, open_weather_city_id) values (2, 7839805);
 insert into open_weather_cities (city_id, open_weather_city_id) values (3, 2063059);
